ab45f9e7b7a76b9b8c6ba29c8d2a8b55efbd6ac2
김한선 <68306239+hansun-hub@users.noreply.github.com>
2023-02-03 12:29:07 +0900

    first

--- src/main/java/com/example/java_coding_test_practice/P11286_절댓값힙.java
+++ src/main/java/com/example/java_coding_test_practice/P11286_절댓값힙.java
@@ -1,2 +1,31 @@
-package com.example.java_coding_test_practice;public class P11286_절댓값힙 {
+package com.example.java_coding_test_practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class P11286_절댓값힙 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> MyQueue = new PriorityQueue<>((o1, o2) -> {
+          int first_abs = Math.abs(o1);
+          int second_abs = Math.abs(o2);
+          if(first_abs == second_abs)
+              return o1 > o2 ? 1 : -1;
+          else
+              return first_abs - second_abs;
+        });
+        for(int i=0;i<N;i++) {
+            int request = Integer.parseInt(br.readLine());
+            if(request == 0) {
+                if(MyQueue.isEmpty())
+                    System.out.println("0");
+                else System.out.println(MyQueue.poll());
+            }else{
+                MyQueue.add(request);
+            }
+        }
+    }
 }
