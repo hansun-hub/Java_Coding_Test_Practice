ee6d80ae204778b66b637218551984823a9b7666
hansunhub <hansun33@naver.com>
2023-04-14 21:40:57 +0900

    2023-04-14_[정렬]k번째수

--- src/main/java/com/example/java_coding_test_practice/Sort/k번째수.java
+++ src/main/java/com/example/java_coding_test_practice/Sort/k번째수.java
@@ -3,11 +3,14 @@ package com.example.java_coding_test_practice.Sort;
 import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
+        int[] answer = new int[commands.length];//commands의 길이만큼 answer생성
         for(int i=0;i<commands.length;i++){
-            int[] temp = new int[commands[i][1]-(commands[i][0]-1)];
-            for(int j= 0; j < temp.length; j++){
-                temp[j] = array[j+(commands[i][0]-1)];
+            int[] temp = new int[commands[i][1]-(commands[i][0]-1)];//j-(i-1)
+            //전체적으로 필요한 temp, commands가 공통적으로 사용
+            //따라서 commands 수 만큼 그 commands가 필요한 개수만큼 자른 배열로 temp[] 선언
+
+            for(int j= 0; j < temp.length; j++){ //temp 배열의 개수만큼 반복
+                temp[j] = array[j+(commands[i][0]-1)]; //
             }
             Arrays.sort(temp);
             answer[i] = temp[commands[i][2]-1];
@@ -16,3 +19,6 @@ class Solution {
         return answer;
     }
 }
+
+//list 는 size
+//배열은 length
