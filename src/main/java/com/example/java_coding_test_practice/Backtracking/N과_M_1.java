b1fc96bba2ba75616c4d888105e52cc2083c3166
hansun-hub <68306239+hansun-hub@users.noreply.github.com>
2023-05-09 07:27:44 +0900

    2023-05-04_[백트래킹] N과 M_ 2번째 풀이

--- src/main/java/com/example/java_coding_test_practice/Backtracking/N과_M_1.java
+++ src/main/java/com/example/java_coding_test_practice/Backtracking/N과_M_1.java
@@ -25,6 +25,7 @@ public class N과_M_1{
     }
 
     public static void dfs(int n,int m,int depth){
+        //재귀 깊이가 M과 같아지면 탐색과정에서 담았던 배열을 출력
         if(depth == m){
             for(int val:arr){
                 sb.append(val).append(' ');
@@ -33,10 +34,13 @@ public class N과_M_1{
             return;
         }
         for(int i=0;i<n;i++){
+            //만약 해당 노드를 방문하지 않았더라면?
             if(!visit[i]){
-                visit[i]=true;
-                arr[depth]=i+1;
-                dfs(n,m,depth+1);
+                visit[i]=true;      //해당 노드를 방문상태로 변경
+                arr[depth]=i+1;     //해당 깊이를 index로 하여 i+1 값 저장
+                dfs(n,m,depth+1);//다음 자식 노드 방문을 위해 depth 1 증가 시키면서 재귀호출
+
+                //자식 노드 방문이 끝나고 돌아오면 방문노드를 방문하지 않은 상태로 변경
                 visit[i]=false;
             }
         }
